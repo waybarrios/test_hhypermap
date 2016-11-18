0f0368a0b994162cd249046159f7a668b73730fd
Way Barrios <waybarrios@gmail.com>
2016-11-18 08:40:01 -0500

    checking layer from hhypermap

--- check_layers.py
+++ check_layers.py
@@ -2,7 +2,7 @@ import requests
 import os
 import yaml
 from IPython.display import display, HTML
-import csv 
+import csv
 
 type_data = 'records_uuid'
 with open('{0}.txt'.format(type_data), 'rb') as file:
@@ -16,31 +16,33 @@ if not os.path.isdir(type_data):
 #0 if is a valid mapproxy config - it returns a yaml, 1 if its invalid
 valid_config= [0 for i in range (len(list_uuid))]
 #0 if it is between -180,-90, 180, 90, 1 if it is outside that range or is None
-valid_bbox = [0 for i in range (len(list_uuid))
+valid_bbox = [0 for i in range (len(list_uuid))]
 
 test_url = 'http://hh.worldmap.harvard.edu/registry/hypermap/layer'
 
 for i in range(len(list_uuid)):
-    response = requests.get('{0}/{1}/map/config'.format(test_url, list_uuid[i]))
-    
-    try:
+   print "Progress {0} / {1}".format(i+1,len(list_uuid))
+   print "Percentage: {0} %".format((i*1.0+1.0)/len(list_uuid)*100.0)
+   response = requests.get('{0}/{1}/map/config'.format(test_url, list_uuid[i]))
+
+   try:
         yaml_text = yaml.load(response.content)
         wms = yaml_text['services']['wms']
         bbox = wms['bbox'].split(',')
         bbox_x0 = bbox[0]
-       	bbox_y0 = bbox[1]
+        bbox_y0 = bbox[1]
         bbox_x1 = bbox[2]
         bbox_y1 = bbox[3]
         if bbox_x0=='None' or bbox_y0=='None' or bbox_x1=='None' or bbox_y1=='None':
             valid_bbox[i]=1
         else:
-        	bbox_x0 = float(bbox_x0)
-       		bbox_y0 = float(bbox_y0)
-        	bbox_x1 = float(bbox_x1)
-        	bbox_y1 = float(bbox_y1)
-            if (abs(bbox_x0)>180.0) and (abs(bbox_x1)>180.0) and (abs(bbox_y0)>90) and (abs(bbox_y1)>90):
+           bbox_x0 = float(bbox_x0)
+           bbox_y0 = float(bbox_y0)
+           bbox_x1 = float(bbox_x1)
+           bbox_y1 = float(bbox_y1)
+           if (abs(bbox_x0)>180.0) and (abs(bbox_x1)>180.0) and (abs(bbox_y0)>90) and (abs(bbox_y1)>90):
                 valid_bbox[i]=1
-        
+
     except Exception as e:
         #it means it is invalid from mapproxy config
         valid_config[i]=1
